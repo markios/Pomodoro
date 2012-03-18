cb845e1b3eed1fe8095a29c64ecbb4d149ba9311
mark.kellett <markjkellett@gmail.com>
2012-03-18 14:20:41 +0000

    further updates

--- js/views/statistics.js
+++ js/views/statistics.js
@@ -3,18 +3,48 @@
 	var statistics = APP.namespace('APP.statistics');
 
 	statistics.view = APP.parentView.extend({
+		_tasksElements : [],
 		_updateTaskStats : function(){
-			var todos = APP.models.Todos;
-			this._totalTasks.html(todos.length).removeClass('data-pulse').addClass('data-pulse');
-			this._completeTasks.html(todos.done().length).removeClass('data-pulse').addClass('data-pulse');
-			this._inProgressTasks.html(todos.remaining().length).removeClass('data-pulse').addClass('data-pulse');
+			
+			var that = this;
+
+			$.each(this._tasksElements, function(index, element){
+				
+				var $taskStat = that[element].el,
+				    amount = that[element].getData();
+
+				if(+$taskStat.html() === amount) return;
+				var $clone = $taskStat
+								.clone()
+								.html(amount);
+
+			    $taskStat
+			    	.before($clone)
+			    	.remove();
+
+			    that[element].el = $clone;
+			});
 		},
 		initialize: function() {
 
-		   this._totalTasks = $('#total_tasks', this.el);
-		   this._completeTasks = $('#complete_tasks', this.el);
-		   this._inProgressTasks = $('#inProgress_tasks', this.el);
-		   
+		   this._totalTasks = { el : $('#total_tasks', this.el) };
+		   this._totalTasks.getData = function(){
+		   	   return window.APP.models.Todos.length;
+		   };
+		   this._tasksElements.push("_totalTasks");
+
+		   this._completeTasks = { el : $('#complete_tasks', this.el) };
+		   this._completeTasks.getData = function(){
+               return window.APP.models.Todos.done().length;
+		   }; 
+		   this._tasksElements.push("_completeTasks");		   
+
+		   this._inProgressTasks = { el : $('#inProgress_tasks', this.el) };
+		   this._inProgressTasks.getData = function(){
+               return window.APP.models.Todos.remaining().length;
+		   };
+		   this._tasksElements.push("_inProgressTasks");		   		   
+
 		   var todos = APP.models.Todos;
 		   todos.bind('add', $.proxy(this._updateTaskStats, this));
 		   todos.bind('change', $.proxy(this._updateTaskStats, this));
